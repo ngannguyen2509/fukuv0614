0e54f45ef79bf6b6f9c3e96cc1331662e113e55e
ngannguyen2509 <167387765+ngannguyen2509@users.noreply.github.com>
2024-06-14 13:52:56 +0900

    fukushyu

--- fukuv0614/Form1.cs
+++ fukuv0614/Form1.cs
@@ -2,9 +2,27 @@ namespace fukuv0614
 {
     public partial class Form1 : Form
     {
+        Random random = new Random();
+        int ans=0;
+        int count = 0;
+        int yosou;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            count++;
+            MessageBox.Show("$\"{count}‰ñ–Ú‚Ì’§í\"");
+
+           
         }
     }
 }
